ec8117f0beca4554b01312a0db47e80000dca5b4
josepeha <joseperazahays@gmail.com>
2023-06-15 14:24:03 -0700

    init: installed ucum library

--- playground-2.js
+++ playground-2.js
@@ -1,147 +1,91 @@
 const admin = require('firebase-admin');
-const {SerializedDocumentArray, SerializedDocument, fromJSON, toJSON} = require('@healthtree/firestore-join');
-admin.initializeApp()
+admin.initializeApp();
 const db = admin.firestore();
-const collection = db.collection('users')
-const {chunk, isEqual, inRange, min, get, sortBy} = require('lodash')
-const {DateTime, Interval} = require('luxon');
-const moment = require('moment-timezone');
-const fs = require('fs');
-const {Blob, Buffer} = require('node:buffer');
-const JSONStream = require('JSONStream')
-const split = require('split')
+const ucum = require('@lhncbc/ucum-lhc');
+const ucumUtils = ucum.UcumLhcUtils.getInstance();
+const {SerializedDocumentArray, SerializedDocument} = require('@healthtree/firestore-join');
 
-function createBigJson(array) {
-  // const arr = [...Array(5).keys()]
-  // const arr = [...Array(2).keys()];
-
-  const writeStream = fs.createWriteStream('toDelete.json');
-  writeStream.write('[')
-  array.forEach((e, index) => {
-    let json =  JSON.stringify(e)
-
-    if(array.length - 1 !== index) {
-      json+=','
-    }
-    writeStream.write(json)
-  })
-  writeStream.write(']')
-  writeStream.end();
-}
-
-
-function getStartAndEndIndex(textJSONPart, startAtIndex, queue = []){
-  let startIndex;
-  let endIndex
-
-  for (let i = startAtIndex; i < textJSONPart.length; i++) {
-    const s = textJSONPart[i]
-    const lastFound = queue[queue.length -1]
-
-    if(s === '"'){
-      const before = textJSONPart[i -1];
-      if(before === '\\'){
-        // escaped double quotes
-      } else {
-        if(lastFound === '"') {
-          // End of the key or value json definition
-          queue.pop();
-        } else {
-          // start of the key or value json definition
-          queue.push(s);
-        }
+async function checkIfValueQuantityIsWithinRange(valueQuantity, ruleData, observationMappings = []) {
+  let toCompareResult;
+  if (ruleData.unitNotRequired) {
+    toCompareResult = {
+      status: 'succeeded',
+      toVal: valueQuantity.value
+    };
+  } else {
+    // TODO: handle mol unit conversion
+    // validate units (valueQuantity.unit, ruleData.unit)
+    // convert valueQuantity.value to ruleData.unit
+    try {
+      toCompareResult = ucumUtils.convertUnitTo(
+          valueQuantity.unit,
+          valueQuantity.value,
+          ruleData.unit
+      );
+    } catch(err) {
+      // si de error me pide un molecullar weight, pide el mapping para sacarlo
+      // convertUnitTo(fromUnitCode, fromVal, toUnitCode, false, molecularWeight) {
+      if (err) {
+        console.log('okay')
       }
-      continue;
     }
-    const isKeyOrValue = lastFound === '"';
-    if(isKeyOrValue) {
-      continue;
-    };
 
-    const openSymbols = {
-      '{': '}',
-    };
-    const closeSymbols = {
-      '}': '{',
-    };
-    const isOpenSymbol = openSymbols[s];
-    const isCloseSymbol = closeSymbols[s];
-
-    if(isOpenSymbol) { // s is {
-      startIndex = i
-      queue.push(s);
-    } else if(isCloseSymbol) { // s is }
-      endIndex = i
-      queue.pop();
-      return {startIndex, endIndex, queue, allAnalyzed: false}
-    }
   }
-  return {startIndex, queue, allAnalyzed: true}
+
+  if (toCompareResult.status === 'succeeded') {
+    if (Number.isFinite(ruleData.min) && Number.isFinite(ruleData.max)) {
+      return toCompareResult.toVal > ruleData.min && toCompareResult.toVal < ruleData.max;
+    } else if (Number.isFinite(ruleData.min)) {
+      return toCompareResult.toVal > ruleData.min;
+    } else if (Number.isFinite(ruleData.max)) {
+      return toCompareResult.toVal < ruleData.max;
+    } else {
+      throw 'expected value ranges';
+    }
+  } else {
+    return false;
+  }
+};
+
+const valueQuantity = {
+  unit: 'nmol/L',
+  value: 500
 }
-
-function readBigJson() {
-  return new Promise((resolve, reject) => {
-    const array = []
-    let queue = [];
-    const inputStream = fs.createReadStream(__dirname + '/toDelete.json')
-    let buf = '';
-    let startIndex, endIndex;
-    inputStream.on('data', (chunk) => {
-      let indexesInfo;
-      while(!indexesInfo?.allAnalyzed) {
-        let startAt = 0;
-        if(!indexesInfo) { // first iteration on new chunk
-          startAt = buf.length;
-          buf += chunk.toString(); // when data is read, stash it in a string buffer
-        }
-        indexesInfo = getStartAndEndIndex(buf, startAt, queue);
-        queue = indexesInfo.queue;
-        if(Number.isFinite(indexesInfo.startIndex)) {
-          startIndex = indexesInfo.startIndex;
-        }
-        if(Number.isFinite(indexesInfo.endIndex)) {
-          endIndex = indexesInfo.endIndex;
-        }
-        if(Number.isFinite(startIndex) && Number.isFinite(endIndex))  {
-          const substring = buf.substring(startIndex, endIndex + 1);
-          const json = fromJSON(substring, db)
-          array.push(json)
-          buf =  buf.substring(endIndex + 1);
-          startIndex = undefined;
-          endIndex = undefined
-        }
-      }
-    })
-    inputStream.on('close' , ()=> {
-      inputStream.close();
-      resolve(array)
-    })
-    inputStream.on('error' , (e)=> {
-      inputStream.close();
-      reject(e);
-    });
-  })
-}
-
-
-
-/**
- *
- * @returns {Promise<void>}
- */
+const ruleData = {
+  resourceToUse: 'mappings',
+      operator: `INCLUDES_OR`,
+      min: 5.51,
+      unit: `g/dL`,
+      mappings: [
+        db.doc(
+            '/apps/curehub/observationMappings/8c8904d0a7b5ae8c4d2bac4bffdc2ff8e5cbd69d0ec716da7c63c8a84d48f4bb'
+        ),
+        db.doc(
+            '/apps/curehub/observationMappings/96cf450f2578b40ddbf0b524fe8c41d73291f6ef6c190793d0d5c2e9f4b17d3d'
+        ),
+        db.doc(
+            '/apps/curehub/observationMappings/08759cd69999593a2567cf2324dc0efbbef7879cff524f04c106980b8a4db597'
+        ),
+        db.doc(
+            '/apps/curehub/observationMappings/d1b71ef6c01de8f2adc7f229aad6a93c0db1cf9cba860c11d2069e85ed0037f1'
+        ),
+        db.doc(
+            '/apps/curehub/observationMappings/e88b5563e0913cf1c726423702ce3e7956f7cfbaf9b6241fa59edb22fcb33dd7'
+        )
+      ]
+};
+const observationMappings = [
+  db.doc(
+      '/apps/curehub/observationMappings/96cf450f2578b40ddbf0b524fe8c41d73291f6ef6c190793d0d5c2e9f4b17d3d'
+  ),
+  db.doc(
+      '/apps/curehub/observationMappings/08759cd69999593a2567cf2324dc0efbbef7879cff524f04c106980b8a4db597'
+  ),
+]
 
 async function main() {
   try {
-    /*
-    string Done
-
-    number
-    obj
-    array
-     */
-    createBigJson([{t: 125.54}, {a:'hello'}])
-    const array = await readBigJson()
-    console.log(array)
+    await checkIfValueQuantityIsWithinRange(valueQuantity, ruleData, observationMappings);
   } catch (e) {
     console.error(e)
   }
